fb01e69b9cf4687eca5741234a04e04ee9bb680f
ZionSung <1122overcomer@gmail.com>
2022-10-18 16:58:27 +0800

    Add transformation in alphazero

--- czf/learner/replay_buffer/alphazero_replay_buffer.py
+++ czf/learner/replay_buffer/alphazero_replay_buffer.py
@@ -4,6 +4,10 @@ from itertools import zip_longest
 
 from czf.learner.replay_buffer.replay_buffer import ReplayBuffer
 
+import random
+import numpy as np
+import czf.env.czf_env
+
 AlphaZeroTransition = namedtuple('AlphaZeroTransition', [
     'observation',
     'policy',
@@ -35,6 +39,8 @@ class AlphaZeroReplayBuffer(ReplayBuffer):
         sample_states,
         observation_config,
         capacity,
+        use_transformation,
+        game,
     ):
         super().__init__(
             num_player,
@@ -45,12 +51,35 @@ class AlphaZeroReplayBuffer(ReplayBuffer):
             capacity,
         )
         self._buffer = deque(maxlen=capacity)
+        self._use_transformation = use_transformation
+        self._game = czf.env.czf_env.load_game(game)
 
     def __len__(self):
         return len(self._buffer)
 
     def __getitem__(self, index):
         transition = self._buffer[index]
+
+        to_ndarray = lambda x, dtype=np.float32: np.array(np.frombuffer(x, dtype=dtype))
+        to_bytes = lambda x, dtype=np.float32: np.array(x, dtype=dtype).tobytes()
+
+        if self._use_transformation:
+            # transformation type
+            transformation_type = random.randint(0, self._game.num_transformations - 1)
+            # observation
+            observation_list = self._game.transform_observation(to_ndarray(transition.observation),
+                                                                transformation_type)
+            transform_observation = to_bytes(observation_list)
+            # policy
+            policy_list = self._game.transform_policy(to_ndarray(transition.policy),
+                                                      transformation_type)
+            transform_policy = to_bytes(policy_list)
+            # return
+            return [
+                transform_observation,
+                transform_policy,
+                transition.value,
+            ]
         return [
             transition.observation,
             transition.policy,
